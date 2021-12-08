95e1d88af283a64883055b2f0240cd917b293d39
Nicola La Rocca <nicola_larocca@xantrix.it>
2021-12-08 17:42:32 +0100

    Aggiunto Footer e Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ArticoliComponent } from './articoli/articoli.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module';
+import { ErrorComponent } from './error/error.component';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { ErrorComponent } from './error/error.component';
-import { ArticoliComponent } from './articoli/articoli.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { ArticoliComponent } from './articoli/articoli.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
