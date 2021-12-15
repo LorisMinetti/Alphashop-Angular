8e8c38ea415b4f884311a5a48b71d3a91466e2d3
Nicola La Rocca <nicola_larocca@xantrix.it>
2021-12-15 17:17:01 +0100

    Creata Griglia Articoli Con Eventi

--- src/app/pages/grid-articoli/grid-articoli.component.ts
+++ src/app/pages/grid-articoli/grid-articoli.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ArticoliService } from 'src/services/articoli.service';
+import { IArticoli } from 'src/app/models/Articoli';
+
 @Component({
   selector: 'app-grid-articoli',
   templateUrl: './grid-articoli.component.html',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridArticoliComponent implements OnInit {
 
-  constructor() { }
+  articoli$ : IArticoli[] = [];
+
+  constructor(private articoliService: ArticoliService) { }
 
   ngOnInit(): void {
+
+    this.articoli$ = this.articoliService.getArticoli();
+    console.log(this.articoli$);
+
+  }
+
+  handleEdit = (codart : string) => {
+    console.log("Cliccato tasto modifica del codice " + codart);
+
+
+  }
+
+  handleDelete = (codart : string) => {
+    console.log("Cliccato tasto elimina del codice " + codart);
+
+    this.articoli$.splice(this.articoli$.findIndex(x => x.codart === codart), 1);
+    console.log(this.articoli$);
+
   }
 
 }
