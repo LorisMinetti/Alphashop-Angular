2076b0bfc1488881af8044f945407eaa3e0d204e
Nicola La Rocca <nicola_larocca@xantrix.it>
2021-12-19 12:16:33 +0100

    Creato servizio saluti-data

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
+import { SalutiDataService } from 'src/services/data/saluti-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -14,11 +15,14 @@ export class WelcomeComponent implements OnInit {
   titolo: string = "Benvenuti in Alphashop";
   sottotitolo: string = "Visualizza le offerte del giorno";
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private salutiSrv : SalutiDataService) { }
 
   ngOnInit(): void {
 
     this.utente = this.route.snapshot.params['userid'];
   }
+
+  getSaluti = () : void => console.log(this.salutiSrv.getSaluti());
+
 
 }
