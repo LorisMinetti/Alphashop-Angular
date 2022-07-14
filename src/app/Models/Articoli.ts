1175ac40ec37b08a48e413d8abfe1d9910b0ddf1
LorisMinetti <92784723+LorisMinetti@users.noreply.github.com>
2022-07-14 11:22:09 +0200

    Problem fixed
    Risolti i problemi di generazione righe con l' ngFor.
    Risolti i problemi di classi extra.
    Risolti i problemi con le PIPE.

---
+++ src/app/Models/Articoli.ts
@@ -0,0 +1,10 @@
+export interface IArticoli {
+  codArt: string
+  descrizione: string
+  um: string
+  pzCart: number
+  peso: number
+  prezzo: number
+  active: boolean
+  data: Date
+}
--- src/app/articoli/articoli.component.ts
+++ src/app/articoli/articoli.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IArticoli } from '../Models/Articoli';
 
 @Component({
   selector: 'app-articoli',
@@ -7,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticoliComponent implements OnInit {
 
-  articoli = [
-    {codArt : '014600301', descrizione : 'BARILLA FARINA 1 KG', um : 'PZ', pzCart : 24, peso : 1, prezzo : 1.09},
-    {codArt : "013500121", descrizione : "BARILLA PASTA GR.500 N.70 1/2 PENNE", um : "PZ", pzCart : 30, peso : 0.5, prezzo : 1.3},
-    {codArt : "007686402", descrizione : "FINDUS FIOR DI NASELLO 300 GR", um : "PZ", pzCart : 8, peso : 0.3, prezzo : 6.46},
-    {codArt : "057549001", descrizione : "FINDUS CROCCOLE 400 GR", um : "PZ", pzCart : 12, peso : 0.4, prezzo : 5.97}
+  articoli: IArticoli[] = [
+    {codArt : '014600301', descrizione : 'BARILLA FARINA 1 KG', um : 'PZ', pzCart : 24, peso : 1, prezzo : 1.09, active : true, data : new Date()},
+    {codArt : "013500121", descrizione : "BARILLA PASTA GR.500 N.70 1/2 PENNE", um : "PZ", pzCart : 30, peso : 0.5, prezzo : 1.3, active : true, data : new Date()},
+    {codArt : "007686402", descrizione : "FINDUS FIOR DI NASELLO 300 GR", um : "PZ", pzCart : 8, peso : 0.3, prezzo : 6.46, active : true, data : new Date()},
+    {codArt : "057549001", descrizione : "FINDUS CROCCOLE 400 GR", um : "PZ", pzCart : 12, peso : 0.4, prezzo : 5.97, active : true, data : new Date()}
   ]
 
   constructor() { }
